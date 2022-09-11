2e1ebfafe165da5df2fded004d2dcb8d77fb30e7
Nexe08 <rohitrautela1408@gmail.com>
2022-09-11 15:13:33 +0530

    Implemented maze generation.

--- tile_map.gd
+++ tile_map.gd
@@ -1,54 +1,63 @@
 extends TileMap
+var N = 1
+var E = 2
+var S = 4
+var W = 8
 
-const TILE_SIZE = 64
+var cell_walls = {
+	Vector2(1, 0): E,
+	Vector2(-1, 0): W,
+	Vector2(0, 1): S,
+	Vector2(0, -1): N
+}
 
-var width: int
-var height: int
-
-var playing := false
-var temp_field
-
-onready var screen_size = get_viewport().get_visible_rect().size
+var tile_size = 64
+var width = 25
+var height = 15
 
 func _ready() -> void:
-	width = screen_size.x / 20
-	height = screen_size.y / 20
-	
-	var width_px = width * TILE_SIZE
-	var height_px = height * TILE_SIZE
-	
-	var cam = $Camera2D
-	
-	cam.position = Vector2(width_px, height_px) / 2
-	cam.zoom = Vector2(width_px, height_px) / Vector2(screen_size.x, screen_size.y)
-	
-	temp_field = []
-	for x in range(width):
-		var temp = []
-		for y in range(height):
-			set_cell(x, y, 0)
-			temp.append(0)
-		temp_field.append(temp)
-
-func _process(_delta: float) -> void:
-	_update_field()
-	yield(get_tree().create_timer(.1), "timeout")
-	if Input.is_action_just_pressed("ui_up"):
-		var pos = (get_global_mouse_position() / TILE_SIZE).floor()
-		set_cellv(pos, 1-get_cellv(pos))
+	randomize()
+	tile_size = cell_size
+	make_maze()
 
 
-func _update_field():
-	# update position in temp field
+func check_neighbors(cell, unvisited):
+	# returns an array of unvisited neighboring cell
+	var list = []
+	for n in cell_walls.keys():
+		if cell + n in unvisited:
+			list.append(cell + n)
+	return list
+
+
+func make_maze():
+	var unvisited = [] # array of unvisited tiles
+	var stack = []
+	
+	# fill the map with solide color
+	clear() # clear the time map
 	for x in range(width):
 		for y in range(height):
-			var live_neighbors = 0
-			for x_off in [-1, 0, 1]:
-				for y_off in [-1, 0, 1]:
-					if x_off != y_off or x_off != 0:
-						if get_cell(x + x_off, y + y_off):
-							live_neighbors += 1
-			
-			if get_cell(x, y) == 1:
-				if live_neighbor in  [2, 3]:
-					pass
+			unvisited.append(Vector2(x, y))
+			set_cellv(Vector2(x, y), N|E|S|W)
+	var current = Vector2(0, 0)
+	unvisited.erase(current)
+	
+	# execute recursive backtracker algorithm
+	while unvisited: # loop untile every cell is not visited
+		var neighbors = check_neighbors(current, unvisited)
+		if neighbors.size() > 0: # if there is neighbore
+			# choos any neighbore to next cell that will be current cell
+			var next = neighbors[randi() % neighbors.size()]
+			stack.append(current)
+			# remove wall from the both cell
+			var dir = next - current # nexe and current reffers to there respective cell positon
+			var current_wall = get_cellv(current) - cell_walls[dir]
+			var next_wall = get_cellv(next) - cell_walls[-dir]
+			set_cellv(current, current_wall)
+			set_cellv(next, next_wall)
+			current = next
+			unvisited.erase(current)
+		elif stack:
+			current = stack.pop_back()
+		yield(get_tree(), "idle_frame")
