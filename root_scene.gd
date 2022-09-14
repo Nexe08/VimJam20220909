c1453eb41441cc21c6c94d1e49a386d20b405367
Nexe08 <rohitrautela1408@gmail.com>
2022-09-15 00:59:29 +0530

    camera rotate with ease based on button press delay. rether then snaping on 90 degree + current rotation in degree.

--- root_scene.gd
+++ root_scene.gd
@@ -12,23 +12,24 @@ func _process(delta: float) -> void:
 		Physics2DServer.AREA_PARAM_GRAVITY_VECTOR).y)
 	)
 	
-	if maz_is_rotation == false:
-		if Input.is_action_just_pressed("left"):
-			maz_is_rotation = true
-			var tween = create_tween().set_ease(Tween.EASE_OUT)
-			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees - 90, .5)
-			yield(tween, "finished")
-			maz_is_rotation = false
-		elif Input.is_action_just_pressed("right"):
-			maz_is_rotation = true # -------
-			var tween = create_tween().set_ease(Tween.EASE_OUT)
-			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees + 90, .5)
-#			var dir = deg2rad(camera.rotation_degrees)
-#			Physics2DServer.area_set_param(\
-#				get_viewport().find_world_2d().get_space(), \
-#				Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(sin(dir), cos(dir)))
-			yield(tween, "finished") # -------
-			maz_is_rotation = false # -------
+	if Input.is_action_pressed("left"):
+		camera.rotation_degrees = lerp(camera.rotation_degrees, camera.rotation_degrees - 10, 5 * delta)
+	elif Input.is_action_pressed("right"):
+		camera.rotation_degrees = lerp(camera.rotation_degrees, camera.rotation_degrees + 10, 5 * delta)
+	
+#	if maz_is_rotation == false:
+#		if Input.is_action_just_pressed("left"):
+#			maz_is_rotation = true
+#			var tween = create_tween().set_ease(Tween.EASE_OUT)
+#			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees - 90, .5)
+#			yield(tween, "finished")
+#			maz_is_rotation = false
+#		elif Input.is_action_just_pressed("right"):
+#			maz_is_rotation = true # -------
+#			var tween = create_tween().set_ease(Tween.EASE_OUT)
+#			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees + 90, .5)
+#			yield(tween, "finished") # -------
+#			maz_is_rotation = false # -------
 	
 	var dir = deg2rad(camera.rotation_degrees + 90)
 	Physics2DServer.area_set_param(\
