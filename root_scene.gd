8b9e9289cdca4d86a2140877eec9554484e1c4d2
Nexe08 <rohitrautela1408@gmail.com>
2022-09-14 07:14:48 +0530

    Modifing Physics2dServer Gravity Vectory based on camera rotation.

--- root_scene.gd
+++ root_scene.gd
@@ -17,19 +17,20 @@ func _process(delta: float) -> void:
 			maz_is_rotation = true
 			var tween = create_tween().set_ease(Tween.EASE_OUT)
 			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees - 90, .5)
-			var dir = deg2rad(camera.rotation_degrees)
-			Physics2DServer.area_set_param(\
-				get_viewport().find_world_2d().get_space(), \
-				Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(cos(dir), sin(dir)))
 			yield(tween, "finished")
 			maz_is_rotation = false
 		elif Input.is_action_just_pressed("right"):
 			maz_is_rotation = true # -------
 			var tween = create_tween().set_ease(Tween.EASE_OUT)
 			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees + 90, .5)
-			var dir = deg2rad(camera.rotation_degrees - 90)
+#			var dir = deg2rad(camera.rotation_degrees)
 #			Physics2DServer.area_set_param(\
 #				get_viewport().find_world_2d().get_space(), \
-#				Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(cos(dir), sin(dir)))
+#				Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(sin(dir), cos(dir)))
 			yield(tween, "finished") # -------
 			maz_is_rotation = false # -------
+	
+	var dir = deg2rad(camera.rotation_degrees + 90)
+	Physics2DServer.area_set_param(\
+		get_viewport().find_world_2d().get_space(), \
+		Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(cos(dir), sin(dir)))
