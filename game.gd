3c4ad1efc07200ea59dfa6d1538c1dca23423f5b
Nexe08 <rohitrautela1408@gmail.com>
2022-09-17 20:55:11 +0530

    Implemented background.

--- game.gd
+++ game.gd
@@ -1,18 +1,11 @@
 extends Node2D
 
+#onready var background = $Background
 onready var camera = $MainCamera
 onready var moving_maze_sfx = $MovingMazeSFX
 onready var maz_is_rotation:= false
 
 func _process(delta: float) -> void:
-	$Label.text = str(atan2(Physics2DServer.area_get_param(\
-		get_viewport().find_world_2d().get_space(), \
-		Physics2DServer.AREA_PARAM_GRAVITY_VECTOR).x, 
-		Physics2DServer.area_get_param(\
-		get_viewport().find_world_2d().get_space(), \
-		Physics2DServer.AREA_PARAM_GRAVITY_VECTOR).y)
-	)
-	
 	if Input.is_action_pressed("left"):
 		if not moving_maze_sfx.playing:
 			moving_maze_sfx.play()
@@ -31,22 +24,8 @@ func _process(delta: float) -> void:
 	elif Input.is_action_just_released("right"):
 		moving_maze_sfx.stop()
 	
-#	if maz_is_rotation == false:
-#		if Input.is_action_just_pressed("left"):
-#			maz_is_rotation = true
-#			var tween = create_tween().set_ease(Tween.EASE_OUT)
-#			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees - 90, .5)
-#			yield(tween, "finished")
-#			maz_is_rotation = false
-#		elif Input.is_action_just_pressed("right"):
-#			maz_is_rotation = true # -------
-#			var tween = create_tween().set_ease(Tween.EASE_OUT)
-#			tween.tween_property(camera, "rotation_degrees", camera.rotation_degrees + 90, .5)
-#			yield(tween, "finished") # -------
-#			maz_is_rotation = false # -------
-	
 	var dir = deg2rad(camera.rotation_degrees + 90)
+#	background.rect_rotation = camera.rotation + 90
 	Physics2DServer.area_set_param(\
 		get_viewport().find_world_2d().get_space(), \
 		Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(cos(dir), sin(dir)))
-
