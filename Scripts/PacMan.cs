864bcb58f16933cc60e94a16073a543247537943
Nexe08 <rohitrautela1408@gmail.com>
2022-09-30 17:17:00 +0530

    Implemented PacMan animation.

--- Scripts/PacMan.cs
+++ Scripts/PacMan.cs
@@ -3,5 +3,12 @@ using System;
 
 public class PacMan : RigidBody2D
 {
+    AnimationPlayer animPlayer;
+
+    public override void _Ready()
+    {
+        animPlayer = FindNode("AnimationPlayer") as AnimationPlayer;
+        animPlayer.Play("moving");
+    }
     
 }
